e830f1bb72b928395444600118468d23140f779d
Elie Housseini <eliashousseini@gmail.com>
2020-09-13 11:37:52 +0300

    Displaying the Errors in the form page

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php 
+
+require('user_validator.php');
+
+  if(isset($_POST['submit'])){
+    // validate entries
+    $validation = new UserValidator($_POST);
+    $errors = $validation->validateForm();
+  }
+
 ?>
 
 
@@ -19,10 +28,18 @@
     <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method = 'POST'>
 
       <label for="">Username:</label>
-    <input type="text" name = 'username'>
+    <input type="text" name = 'username' value = "<?php echo htmlspecialchars($_POST['username']) ?? '' ?>">
+    <div class="error">
+      <?php echo $errors['username'] ?? '' ?>
+    </div>
 
     <label>Email:</label>
-    <input type="text" name = 'email'>
+
+    <input type="text" name = 'email' value = "<?php echo htmlspecialchars($_POST['email']) ?? '' ?>">
+
+    <div class="error">
+      <?php echo $errors['email'] ?? '' ?>
+    </div>
 
     <input type="submit" value = 'submit' name = 'submit' >
 
